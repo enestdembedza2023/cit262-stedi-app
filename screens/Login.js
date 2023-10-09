9f6b91fb569ae31ee648ee893a1e2805d06c5a45
Enest Dembedza <enestdembedza@outlook.com>
2023-10-09 12:33:58 +0200

    Add a new line on login.js to show the Hello message with the phone number.

--- screens/Login.js
+++ screens/Login.js
@@ -67,18 +67,18 @@ const Login = ({loggedInState, loggedInStates,setLoggedInState})=>{
                     method:'POST',
                     headers:{
                      'content-type':'application/text'
-                   }
+                    }
                   }
-                  );
-                  const sendTextResponseData = await sendTextResponse.text();
-                  if(sendTextResponse.status!=200){//invalid phone number, send them to the signup page
-                    await Alert.alert("Did you type your number correctly? " + phoneNumber);
-                  } else{
-                    setLoggedInState(loggedInStates.LOGGING_IN);
-                  }
+                );
+                const sendTextResponseData = await sendTextResponse.text();
+                if(sendTextResponse.status!=200){//invalid phone number, send them to the signup page
+                  await Alert.alert("Did you type your number correctly? " + phoneNumber);
+                } else{
+                  setLoggedInState(loggedInStates.LOGGING_IN);
+                }
               }}
             >
-              <Text style={{color:'white'}}>Send</Text>      
+              <Text style={{color:'white'}}>Send</Text>
             </TouchableOpacity>
     
           </View>
